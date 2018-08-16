05c84a857884f0617169d5da48f760eec1cf81e9
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-08-16 05:28:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
- if int % 3 == 0
+ if int % 3 == 1
     "Fizz"
- elsif int % 5 == 0
+ elsif int % 5 == 1
     "Buzz"
- elsif int % 15 == 0
+ elsif int % 15 == 1
     "Fizzbuzz"
   end
 
